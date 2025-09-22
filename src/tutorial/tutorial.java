1e9fc9fcd9b823184ada1930406afb0c3c37fe06
AntonioEspana <140651448+AntonioEspana@users.noreply.github.com>
2025-09-22 17:03:27 +0200

    Mi primer commit

---
+++ src/tutorial/tutorial.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tutorial;
+
+/**
+ * 
+ */
+public class tutorial {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+	}
+
+}
